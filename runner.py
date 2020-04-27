aa74105a73c2707c8926c6040d359e87fb1ce9ee
Michael Mulholland <G00362383@gmit.ie>
2020-04-27 20:40:37 +0000

    Added a command-line interface - not finished yet

--- runner.py
+++ runner.py
@@ -13,7 +13,24 @@ import unittest
 # import the test_program.py file
 import test_program
 
+# imported for the command-line interface
+import argparse
+
 def main():
+   
+    parser = argparse.ArgumentParser(
+        description = "A program using the Python programming language that\
+                can build a non-deterministic finite automation (NFA) from\
+                a regular expression. This program will use the NFA to check\
+                if the regular expression matches any given string of text."        
+    )
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-v", "--verbose", help="On option 2 only - pre-written tests", action="store_true")
+    group.add_argument("-q", "--quite", help="On option 2 only - pre-written tests", action="store_true")
+
+    args = parser.parse_args()
+    
     while True:
         # user to select one of three options
         print()
@@ -22,7 +39,7 @@ def main():
         print("Enter 2: to test pre-written tests.")
         print("Enter 3: to quit the program.")
         choice = input("Please select one of the above: ")   
-        
+       
         #If yes then ask for the following input and match it
         if choice == '1':
             print()
@@ -39,7 +56,7 @@ def main():
             # if the regular expression regex matches the string.
             # It returns FALSE otherwise
             print("Regex: " + regex, " String: " + s, " Match: ", match(regex, s))
-              
+           
         elif choice == '2':
             # Print out of the tests and expected result - just so the user can see something
             
@@ -49,22 +66,31 @@ def main():
             stringsArr = ["bcccc", "bbb", "abc", "bc", "ccccccb", 
                  "abccd", "a", ""]
             
-            # Nested for loop to compare each index of the regex array with 
-            # every index of the strings array to see if they match
-            for reg in regex:
+            if args.verbose:
+                # Nested for loop to compare each index of the regex array with 
+                # every index of the strings array to see if they match
+                for reg in regex:
+                    print()
+                    for s in stringsArr:
+                        print("Regex: " + reg, " String: " + s, " Match: ", match(reg, s))
+
+            elif args.quite:
+                # Nested for loop to compare each index of the regex array with 
+                # every index of the strings array to see if they match
+                for reg in regex:
+                    print()
+                    for s in stringsArr:
+                        print(reg, ": " + s, " : ", match(reg, s))
+            else: 
                 print()
-                for s in stringsArr:
-                    print("Regex: " + reg, " String: " + s, " Match: ", match(reg, s))
-           
-            print()
-            # the above tests will allow the user to see the tests and expected result
-            # the below test will only return OK if all tests pass or FAILED if one test fails
-            # The below runs unittest from the test_progam.py
-            # https://stackoverflow.com/questions/31559473/run-unittests-from-a-different-file
-            print("unittest from the test_program.py file")
-            print()
-            suite = unittest.TestLoader().loadTestsFromModule(test_program)
-            unittest.TextTestRunner(verbosity=2).run(suite)    
+                # the above tests will allow the user to see the tests and expected result
+                # the below test will only return OK if all tests pass or FAILED if one test fails
+                # The below runs unittest from the test_progam.py
+                # https://stackoverflow.com/questions/31559473/run-unittests-from-a-different-file
+                print("unittest from the test_program.py file")
+                print()
+                suite = unittest.TestLoader().loadTestsFromModule(test_program)
+                unittest.TextTestRunner(verbosity=2).run(suite)    
 
         else:
             print()
